bf976ca006f02f67b88a161e97cc314ce966866b
John <spurneyjohn@gmail.com>
2017-05-17 09:41:48 -0400

    Fix bug, convert rgb image to brg. Also, remove steering angle bias

--- drive.py
+++ drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -60,19 +62,14 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
 
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image = Image.open(BytesIO(base64.b64decode(imgString))) 
+
         image_array = np.asarray(image)
 
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        # Convert RGB PIL image to BGR
+        image_array = image_array[:,:,::-1].copy()
 
-        # Bias constant, exagerates steering angle by a little bit
-        # this constant seems to provide better results
-        bias = 0.1
-        # Logic for the bias constant
-        if steering_angle >= 0:
-            steering_angle += bias
-        else:
-            steering_angle -= bias
+        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
